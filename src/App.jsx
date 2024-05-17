008dc0fcab27a46707eab4191d1d434ff3da0089
Geovanaiviny <ivinylima5@gmail.com>
2024-05-17 16:43:58 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <h1 className="bg-red-700 text-3xl font-bold underline">Hello World!</h1>
+  )
+}
+
+export default App
