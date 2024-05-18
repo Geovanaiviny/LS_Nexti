bfbe0c28ddbd8788ca4bde8a46af0ce3611b5575
Geovanaiviny <ivinylima5@gmail.com>
2024-05-17 23:14:26 -0300

    Instalação e atualização de react

---
+++ my-juniper/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <body> 
+      <h1 className="text-[#C71585] text-3xl font-bold">My litttle babys</h1>
+      <h2 className="text-[#008B8B] text-2xl font-bold">Juniper, Romeu and Groot</h2>
+    </body>
+
+  )
+}
+
+export default App
