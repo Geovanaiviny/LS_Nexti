634247cc7cf0041589d6c4e6ff53117a614163e5
Geovanaiviny <ivinylima5@gmail.com>
2024-05-22 17:17:37 -0300

    ajuste

--- my-juniper/src/componentes_options/Select.jsx
+++ my-juniper/src/componentes_options/Select.jsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React from 'react';
 import Setad from '../assets/img_icon/setad.png'
+
 
 
 const Select = () => {
--- my-juniper/src/components/header.jsx
+++ my-juniper/src/components/header.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from 'react';
 import Logo from "../assets/img/logoh.png"
-
 import Buscar from '../assets/img_icon/buscar.png'
 import User from '../assets/img_icon/user.png'
 // import Select from '../components_options/Select'
-import Select from '../componentes_options/Select';
+import Select from '../componentes_options/select';
 
 
 
 function Header() {
     
-    return(
+    return (
         <>
 
         {/* Header home principal */}
@@ -26,15 +25,16 @@ function Header() {
             <div name="area" className="flex justify-center  items-center space-x-16">
 
                 
-                <form action method="get">
+                
                     <Select />
-                    {/* <select className="bg-[#FFFCFC] border border-[#C0C0C0] rounded-md w-[280px] px-4 h-11 text-[#757575] custom-select">
+
+                    {/* form <select className="bg-[#FFFCFC] border border-[#C0C0C0] rounded-md w-[280px] px-4 h-11 text-[#757575] custom-select">
                         <option className="text-gray-300" selected="selh_p">Áreas de busca</option>
                         <option value="selh_1">Enfermagem</option>
                         <option value="selh_2">Arquitetura</option>
                         <option value="selh_3">Sistemas</option>
                     </select> */}
-                </form>
+                
                 
                 {/* Div específica do input pesquisar e button */}
                 <div name="pesquisah" className="flex space-x-2 items-center">
@@ -55,7 +55,7 @@ function Header() {
         </header>
         </>
 
-    )
+    );
 }
 
 export default Header
