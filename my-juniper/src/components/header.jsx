52f2c6f693470a6d9a979ac0c3b9cff11516b370
Geovanaiviny <ivinylima5@gmail.com>
2024-05-23 12:36:30 -0300

    Criação select

--- my-juniper/src/components/header.jsx
+++ my-juniper/src/components/header.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from 'react';
 import Logo from "../assets/img/logoh.png"
-
 import Buscar from '../assets/img_icon/buscar.png'
 import User from '../assets/img_icon/user.png'
 // import Select from '../components_options/Select'
-import Select from '../selection/Select';
+import Select from '../componentes_options/Select';
 
 
 
 function Header() {
     
-    return(
+    return (
         <>
 
         {/* Header home principal */}
@@ -26,8 +25,10 @@ function Header() {
             <div name="area" className="flex justify-center  items-center space-x-16">
 
                 
-               
-                   <div> <Select /> </div> 
+                <div>
+                <Select />
+                </div>
+                    
                     {/* <select className="bg-[#FFFCFC] border border-[#C0C0C0] rounded-md w-[280px] px-4 h-11 text-[#757575] custom-select">
                         <option className="text-gray-300" selected="selh_p">Áreas de busca</option>
                         <option value="selh_1">Enfermagem</option>
@@ -55,7 +56,7 @@ function Header() {
         </header>
         </>
 
-    )
+    );
 }
 
 export default Header
