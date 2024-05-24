9f7b2fd84f9140da6998720dec461d73b6959081
Geovanaiviny <ivinylima5@gmail.com>
2024-05-24 17:02:23 -0300

    Finalização select e ajuste do pesquisar

--- my-juniper/src/components/Select.jsx
+++ my-juniper/src/components/Select.jsx
@@ -1,10 +1,62 @@
+
 import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import Setad from '../assets/img_icon/setad.png'
+import EditIcon from '@mui/icons-material/Edit';
+import Divider from '@mui/material/Divider';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-export default function BasicMenu() {
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(0.2),
+    minWidth: 260,
+    color:
+    '#808080',
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:hover': {
+        backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.hoverOpacity),
+        color: '#E68C3A',
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
+
+export default function CustomizedMenus() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -15,51 +67,69 @@ export default function BasicMenu() {
   };
 
   return (
-    <div className="bg-[#FFFCFC] border border-[#C0C0C0] rounded-md h-11 w-[280px] flex items-center space-x-4">
+    <div>
       <Button
         sx={{ 
-          // color: '#898989',
-          color: '#AAAAAA',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          width: '100%', // Faz com que o botão ocupe toda a largura do div pai
-          textTransform: 'none', // Opcional: mantém o texto como está, sem transformações para maiúsculas
-          padding: '0 16px',
-          fontFamily: '"worksans", sans-serif',
-          fontSize: '1rem',
-          marginTop: '3px',
-        }}
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
+            background: '#FFFCFC',
+            height: '40px',
+            width: '260px',
+            border: '1px solid #C0C0C0',
+            color: '#A9A9A9',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            textTransform: 'none', // Opcional: mantém o texto como está, sem transformações para maiúsculas
+            padding: '0 16px',
+            fontFamily: '"worksans", sans-serif',
+            fontSize: '1rem',
+            marginTop: '3px',
+            borderRadius: '6px',
+            '&:hover': {
+                backgroundColor: '#FFFCFC', // Define a cor de fundo para o estado de hover
+                borderColor: '#E68C3A', // Opcional: também altera a cor da borda no hover
+              },
+              ...(open && {
+                backgroundColor: '#FFFCFC',
+                borderColor: '#E68C3A',
+              }),
+              
+             }}
+        id="demo-customized-button"
+        aria-controls={open ? 'demo-customized-menu' : undefined}
         aria-haspopup="true"
         aria-expanded={open ? 'true' : undefined}
+        variant="contained"
+        disableElevation
         onClick={handleClick}
-       >
+        endIcon={<KeyboardArrowDownIcon />}
+      >
         Área de conhecimento
-        <figure><img className=" h-[22px]" src={Setad} alt="" /></figure>
       </Button>
-      <Menu 
-        sx={{
-          width: '280px',
-          display: 'flex',
+      <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button',
         }}
-        id="basic-menu"
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
       >
-        <MenuItem sx={{
-          width: '300px',
-          display: 'flex',
-          alignItems: 'Center',
-        }} onClick={handleClose}>Tecnologia</MenuItem>
-        <MenuItem onClick={handleClose}>Arquitetura</MenuItem>
-        <MenuItem onClick={handleClose}>Engenharia</MenuItem>
-      </Menu>
+        <MenuItem className="w-[260px]" onClick={handleClose} disableRipple>
+          
+          Tecnologia
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          
+          Arquitetura
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          Engenharia
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          Enfermagem
+        </MenuItem>
+      </StyledMenu>
     </div>
   );
 }
+
--- my-juniper/src/components/header.jsx
+++ my-juniper/src/components/header.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/img/logoh.png"
 import Buscar from '../assets/img_icon/buscar.png'
 import User from '../assets/img_icon/user.png'
 // import Select from '../components_options/Select'
-import BasicMenu from './Select';
+import StyledMenu from './Select';
 
 
 function Header() {
@@ -12,26 +12,26 @@ function Header() {
         <>
 
         {/* Header home principal */}
-        <header
-            className="bg-[#F4F2EF] h-32 w-[100%] flex items-center  //justify-between  ">
+        <header className="bg-[#F4F2EF] h-32 w-[100%] flex items-center //justify-center space-x-10">
 
             {/* Logo do header home principal */}
             <figure name="logoh" className="flex items-center">
                 <img className="h-28" src={Logo }/>
             </figure>
             
-                <div className="">
-                <BasicMenu />
+                <div className="flex">
+                {/* <BasicMenu /> */}
+                <StyledMenu />
                 </div>
                     
             {/* Div do select, input e login */}
-            <div name="area" className="flex justify-center  items-center space-x-16">
+            <div name="area" className="flex //justify-center justify-between items-center  //space-x-16">
                 
                 {/* Div específica do input pesquisar e button */}
                 <div name="pesquisah" className="flex space-x-2 items-center">
-                    <input type="text" className="bg-[#FFFCFC] border border-[#C0C0C0] rounded-md w-[280px] px-4 h-11 font-worksans text-base" placeholder="Pesquisar.."/>
-                    <button className="bg-[#E68C3A] w-[74px] h-[41px] rounded-md" type="pheader">
-                        <figure> <img className="h-[22px] px-[24px]" src={Buscar }/></figure>
+                    <input type="text" className="bg-[#FFFCFC] border border-[#C0C0C0] rounded-md w-[260px] px-4 h-10 text-[#757575] hover:bg-sky-700" placeholder="Pesquisar.."/>
+                    <button className="bg-[#E68C3A] w-[74px] h-[38px] rounded-md" type="pheader">
+                        <figure> <img className="h-[20px] px-[24px]" src={Buscar }/></figure>
                     </button>
                 </div>
                 
